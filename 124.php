d439633d1acc24248f4791ba251749b03d445508
yaocuixia <1946854552@qq.com>
2017-06-30 14:56:38 +0800

    MyBlog

---
+++ 124.php
@@ -0,0 +1,3 @@
+<php
+echo "ok";
+echo "ok";
---
+++ js/index.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Administrator on 2016/9/23.
+ */
+$(function(){
+    var cur_status = "less";
+    $.extend({
+        show_more_init:function(){
+//alert("show_more_init!");
+            var charNumbers=$(".content").html().length;//总字数
+            var limit=250;//显示字数
+            if(charNumbers>limit)
+            {
+                var orgText=$(".content").html();//原始文本
+                var orgHeight=$(".content").height();//原始高度
+                var showText=orgText.substring(0,limit);//最终显示的文本
+                $(".content").html(showText);
+                var contentHeight=$(".content").height();//截取内容后的高度
+                $(".switch").click(
+                    function() {
+                        if(cur_status == "less"){
+                            $(".content").height(contentHeight).html(orgText).animate({ height:orgHeight}, { duration: "slow" });
+                            $(this).html("收缩");
+                            cur_status = "more";
+                        }else{
+                            $(".content").height(orgHeight).html(showText).animate({ height:contentHeight}, { duration: "fast" });
+                            $(this).html("......");
+                            cur_status = "less";
+                        }
+                    }
+                );
+            }
+            else
+            {
+                $(".switch").hide();
+            }
+        }
+    });
+    $('.switch').ready(function(){
+        $.show_more_init();
+    });
+    $("#text1").click(function(){
+        window.location.href='1.html';
+    });
+    $("#text2").click(function(){
+        window.location.href='2.html';
+    });
+    $("#text3").click(function(){
+        window.location.href='3.html';
+    });
+    $("#text4").click(function(){
+        window.location.href='4.html';
+    });
+    setInterval(function(){
+        $("#text5").click(function(){
+            window.location.href='5.html';
+        });
+
+    },1000);
+    var _index=0;
+    $("#bottom_left ul li").mouseenter(function(){
+       var oImg= $(this).find("img").attr('src');
+        var _index=$(this).index();//alert(_index);
+
+
+
+    });
+
+});
